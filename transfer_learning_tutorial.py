b00e28cec0873a652f68d48dbe2d462e12a11f6b
Timmy-Fang <33807622+Timmy-Fang@users.noreply.github.com>
2018-04-09 10:44:32 +0800

    Add files via upload

--- transfer_learning_tutorial.py
+++ transfer_learning_tutorial.py
@@ -48,7 +48,7 @@ import time
 import os
 import copy
 
-plt.ion()   # interactive mode
+#plt.ion()   # interactive mode
 
 ######################################################################
 # Load Data
@@ -106,26 +106,26 @@ use_gpu = torch.cuda.is_available()
 # Let's visualize a few training images so as to understand the data
 # augmentations.
 
-def imshow(inp, title=None):
-    """Imshow for Tensor."""
-    inp = inp.numpy().transpose((1, 2, 0))
-    mean = np.array([0.485, 0.456, 0.406])
-    std = np.array([0.229, 0.224, 0.225])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-    plt.pause(0.001)  # pause a bit so that plots are updated
-
-
-# Get a batch of training data
-inputs, classes = next(iter(dataloaders['train']))
-
-# Make a grid from batch
-out = torchvision.utils.make_grid(inputs)
-
-imshow(out, title=[class_names[x] for x in classes])
+#def imshow(inp, title=None):
+#    """Imshow for Tensor."""
+#    inp = inp.numpy().transpose((1, 2, 0))
+#    mean = np.array([0.485, 0.456, 0.406])
+#    std = np.array([0.229, 0.224, 0.225])
+#    inp = std * inp + mean
+#    inp = np.clip(inp, 0, 1)
+#    plt.imshow(inp)
+#    if title is not None:
+#        plt.title(title)
+#    plt.pause(0.001)  # pause a bit so that plots are updated
+#
+#
+## Get a batch of training data
+#inputs, classes = next(iter(dataloaders['train']))
+#
+## Make a grid from batch
+#out = torchvision.utils.make_grid(inputs)
+#
+#imshow(out, title=[class_names[x] for x in classes])
 
 
 ######################################################################
@@ -222,33 +222,33 @@ def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
 # Generic function to display predictions for a few images
 #
 
-def visualize_model(model, num_images=6):
-    was_training = model.training
-    model.eval()
-    images_so_far = 0
-    fig = plt.figure()
-
-    for i, data in enumerate(dataloaders['val']):
-        inputs, labels = data
-        if use_gpu:
-            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
-        else:
-            inputs, labels = Variable(inputs), Variable(labels)
-
-        outputs = model(inputs)
-        _, preds = torch.max(outputs.data, 1)
-
-        for j in range(inputs.size()[0]):
-            images_so_far += 1
-            ax = plt.subplot(num_images//2, 2, images_so_far)
-            ax.axis('off')
-            ax.set_title('predicted: {}'.format(class_names[preds[j]]))
-            imshow(inputs.cpu().data[j])
-
-            if images_so_far == num_images:
-                model.train(mode=was_training)
-                return
-    model.train(mode=was_training)
+#def visualize_model(model, num_images=6):
+#    was_training = model.training
+#    model.eval()
+#    images_so_far = 0
+#    fig = plt.figure()
+#
+#    for i, data in enumerate(dataloaders['val']):
+#        inputs, labels = data
+#        if use_gpu:
+#            inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda())
+#        else:
+#            inputs, labels = Variable(inputs), Variable(labels)
+#
+#        outputs = model(inputs)
+#        _, preds = torch.max(outputs.data, 1)
+#
+#        for j in range(inputs.size()[0]):
+#            images_so_far += 1
+#            ax = plt.subplot(num_images//2, 2, images_so_far)
+#            ax.axis('off')
+#            ax.set_title('predicted: {}'.format(class_names[preds[j]]))
+#            imshow(inputs.cpu().data[j])
+#
+#            if images_so_far == num_images:
+#                model.train(mode=was_training)
+#                return
+#    model.train(mode=was_training)
 
 ######################################################################
 # Finetuning the convnet
@@ -286,7 +286,7 @@ model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
 ######################################################################
 #
 
-visualize_model(model_ft)
+#visualize_model(model_ft)
 
 
 ######################################################################
@@ -337,7 +337,7 @@ model_conv = train_model(model_conv, criterion, optimizer_conv,
 ######################################################################
 #
 
-visualize_model(model_conv)
+#visualize_model(model_conv)
 
-plt.ioff()
-plt.show()
+#plt.ioff()
+#plt.show()
